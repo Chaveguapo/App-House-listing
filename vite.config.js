f5213e77dbf22823f57571e6683aabc6de48c8be
app-house-listing <catakeas@gmail.com>
2024-07-15 18:36:52 +0200

    test configuration

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    environment: 'jsdom' // Specify JSDOM as the test environment
+  },
+
   plugins: [vue()],
   server: 8080,
   resolve: {
